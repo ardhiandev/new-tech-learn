c9bf7b7ed3ee95f15600054b52e33c7821e8216a
ardhiandev <ardhiandev@gmail.com>
2022-03-17 14:45:56 +0700

    init learn new tech

---
+++ ES7.js
@@ -0,0 +1,8 @@
+
+// replace all
+const str = "ardhian adalah anak yang rajin menabung dan ardhian tidak sombong"
+console.log(str.replaceAll("ardhian","andy"))
+
+// _ for number or unicode (easy to read)
+var numb = 1_000_000
+console.log(numb)
